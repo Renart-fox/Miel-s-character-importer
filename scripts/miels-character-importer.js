a11d69f824e1f079bcc8a6be9d027a30461f54b6
Kévin Naudin <naudin.kevin@outlook.fr>
2024-03-26 22:56:08 +0100

    Added vampire character importer.

---
+++ scripts/miels-character-importer.js
@@ -0,0 +1,383 @@
+var vEnum = null;
+import("./vampire_enum.js").then((module) => {
+  vEnum = module;
+});
+
+const importVtm5e = async (data) => {
+  let cgObj = data.cg_obj;
+  let type = "vampire";
+  switch (cgObj.type) {
+    case 0:
+      type = "mortal";
+      if (cgObj.goule == true)
+        type = "ghoul";
+      break;
+  }
+
+  var skills = {
+    firearms: { value: 0, bonuses: [] },
+    animalken: { value: 0, bonuses: [] },
+    academics: { value: 0, bonuses: [] },
+    craft: { value: 0, bonuses: [] },
+    leadership: { value: 0, bonuses: [] },
+    finance: { value: 0, bonuses: [] },
+    athletics: { value: 0, bonuses: [] },
+    insight: { value: 0, bonuses: [] },
+    investigation: { value: 0, bonuses: [] },
+    brawl: { value: 0, bonuses: [] },
+    etiquette: { value: 0, bonuses: [] },
+    medicine: { value: 0, bonuses: [] },
+    drive: { value: 0, bonuses: [] },
+    streetwise: { value: 0, bonuses: [] },
+    occult: { value: 0, bonuses: [] },
+    stealth: { value: 0, bonuses: [] },
+    intimidation: { value: 0, bonuses: [] },
+    politics: { value: 0, bonuses: [] },
+    larceny: { value: 0, bonuses: [] },
+    performance: { value: 0, bonuses: [] },
+    science: { value: 0, bonuses: [] },
+    melee: { value: 0, bonuses: [] },
+    persuasion: { value: 0, bonuses: [] },
+    technology: { value: 0, bonuses: [] },
+    survival: { value: 0, bonuses: [] },
+    subterfuge: { value: 0, bonuses: [] },
+    awareness: { value: 0, bonuses: [] }
+  }
+
+  var disciplines = {
+    "animalism": {
+      "name": "WOD5E.VTM.Animalism",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "auspex": {
+      "name": "WOD5E.VTM.Auspex",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "celerity": {
+      "name": "WOD5E.VTM.Celerity",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "dominate": {
+      "name": "WOD5E.VTM.Dominate",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "fortitude": {
+      "name": "WOD5E.VTM.Fortitude",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "obfuscate": {
+      "name": "WOD5E.VTM.Obfuscate",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "potence": {
+      "name": "WOD5E.VTM.Potence",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "presence": {
+      "name": "WOD5E.VTM.Presence",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "protean": {
+      "name": "WOD5E.VTM.Protean",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "sorcery": {
+      "name": "WOD5E.VTM.BloodSorcery",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "oblivion": {
+      "name": "WOD5E.VTM.Oblivion",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "alchemy": {
+      "name": "WOD5E.VTM.ThinBloodAlchemy",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "rituals": {
+      "name": "WOD5E.VTM.Rituals",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    },
+    "ceremonies": {
+      "name": "WOD5E.VTM.Ceremonies",
+      "description": "",
+      "value": 0,
+      "powers": [],
+      "visible": false
+    }
+  }
+
+  skills.firearms.value = parseInt(cgObj.skills[0]);
+  skills.animalken.value = parseInt(cgObj.skills[1]);
+  skills.academics.value = parseInt(cgObj.skills[2]);
+  skills.craft.value = parseInt(cgObj.skills[3]);
+  skills.leadership.value = parseInt(cgObj.skills[4]);
+  skills.finance.value = parseInt(cgObj.skills[5]);
+  skills.athletics.value = parseInt(cgObj.skills[6]);
+  skills.insight.value = parseInt(cgObj.skills[7]);
+  skills.investigation.value = parseInt(cgObj.skills[8]);
+  skills.brawl.value = parseInt(cgObj.skills[9]);
+  skills.etiquette.value = parseInt(cgObj.skills[10]);
+  skills.medicine.value = parseInt(cgObj.skills[11]);
+  skills.drive.value = parseInt(cgObj.skills[12]);
+  skills.streetwise.value = parseInt(cgObj.skills[13]);
+  skills.occult.value = parseInt(cgObj.skills[14]);
+  skills.stealth.value = parseInt(cgObj.skills[15]);
+  skills.intimidation.value = parseInt(cgObj.skills[16]);
+  skills.politics.value = parseInt(cgObj.skills[17]);
+  skills.larceny.value = parseInt(cgObj.skills[18]);
+  skills.performance.value = parseInt(cgObj.skills[19]);
+  skills.science.value = parseInt(cgObj.skills[20]);
+  skills.melee.value = parseInt(cgObj.skills[21]);
+  skills.persuasion.value = parseInt(cgObj.skills[22]);
+  skills.technology.value = parseInt(cgObj.skills[23]);
+  skills.survival.value = parseInt(cgObj.skills[24]);
+  skills.subterfuge.value = parseInt(cgObj.skills[25]);
+  skills.awareness.value = parseInt(cgObj.skills[26]);
+
+  for (var spec of cgObj.specs) {
+    let relatedSkill = vEnum.Skills[parseInt(spec.comp)].name;
+    let specName = spec.spec;
+    let newSpec = {
+      "source": specName,
+      "value": "1",
+      "paths": [
+        "skills." + relatedSkill
+      ],
+      "displayWhenInactive": true,
+      "isActive": false
+    }
+    skills[relatedSkill].bonuses.push(newSpec);
+  }
+
+  for (var discipline of cgObj.disciplines) {
+    let discName = vEnum.Disciplines[discipline.id].name;
+    disciplines[discName].value = discipline.level;
+    disciplines[discName].powers = discipline.powers;
+    disciplines[discName].visible = true;
+
+  }
+
+  let actor = await Actor.create({
+    name: data.name,
+    type: type,
+    img: "icons/svg/mystery-man.svg",
+    system: {
+      "clan": { value: vEnum.Clans[cgObj.clan].name },
+      "headers": {
+        "predator": cgObj.predation,
+        "sire": cgObj.sire,
+        "generation": cgObj.generation
+      },
+      "health": {
+        max: 3 + parseInt(cgObj.attributes.stamina),
+        value: 3 + parseInt(cgObj.attributes.stamina)
+      },
+      "willpower": {
+        max: parseInt(cgObj.attributes.resolve) + parseInt(cgObj.attributes.composure),
+        value: parseInt(cgObj.attributes.resolve) + parseInt(cgObj.attributes.composure)
+      },
+      "abilities": {
+        "strength": {
+          "value": parseInt(cgObj.attributes.strength),
+          "name": "WOD5E.Attributes.Strength"
+        },
+        "charisma": {
+          "value": parseInt(cgObj.attributes.charisma),
+          "name": "WOD5E.Attributes.Charisma"
+        },
+        "intelligence": {
+          "value": parseInt(cgObj.attributes.intelligence),
+          "name": "WOD5E.Attributes.Intelligence"
+        },
+        "dexterity": {
+          "value": parseInt(cgObj.attributes.dexterity),
+          "name": "WOD5E.Attributes.Dexterity"
+        },
+        "manipulation": {
+          "value": parseInt(cgObj.attributes.manipulation),
+          "name": "WOD5E.Attributes.Manipulation"
+        },
+        "wits": {
+          "value": parseInt(cgObj.attributes.cunning),
+          "name": "WOD5E.Attributes.Wits"
+        },
+        "stamina": {
+          "value": parseInt(cgObj.attributes.stamina),
+          "name": "WOD5E.Attributes.Stamina"
+        },
+        "composure": {
+          "value": parseInt(cgObj.attributes.composure),
+          "name": "WOD5E.Attributes.Composure"
+        },
+        "resolve": {
+          "value": parseInt(cgObj.attributes.resolve),
+          "name": "WOD5E.Attributes.Resolve"
+        }
+      },
+      "skills": skills,
+      "bloodPotencyValue": parseInt(cgObj.blood),
+      "blood": {
+        potency: parseInt(cgObj.blood)
+      },
+      "disciplines": disciplines
+    }
+  });
+
+  for (var discipline of cgObj.disciplines) {
+    let discName = vEnum.Disciplines[discipline.id].name;
+    for (var power of discipline.powers) {
+      if (power != "") {
+        var itemData = {
+          name: power,
+          type: "power",
+          img: "/systems/vtm5e/assets/icons/items/discipline.png",
+          system: { discipline: discName }
+        }
+        await Item.createDocuments([itemData], { parent: actor });
+      }
+    }
+  }
+
+}
+
+const importCharacterLoad = async (event) => {
+  let characterData = JSON.parse(event.target.result);
+  let currentSystem = game.system.id;
+
+  switch (characterData.system) {
+    case "3":
+      if (currentSystem != 'vtm5e') {
+        ui.notifications.error("This character is made for Vampire 5e, could not import.");
+        return;
+      }
+      importVtm5e(characterData);
+      break;
+    default:
+      ui.notifications.error("Could not recognize character's system. Import process cancelled.");
+      return;
+  }
+}
+
+const importCharacter = (files) => {
+  for (var file of files) {
+    let reader = new FileReader();
+    reader.addEventListener('load', (e) => { importCharacterLoad(e) });
+    reader.readAsText(file);
+  }
+
+}
+
+const importCharacterPopup = () => {
+  stayOpen = false;
+  files = null;
+  let d = new Dialog({
+    title: "Miel's Character Importer",
+    content: "<p>Please choose the character to import.</p>",
+    buttons: {
+      one: {
+        icon: '<i class="fas fa-check"></i>',
+        label: "Select file...",
+        callback: () => {
+          stayOpen = true;
+          var input = document.createElement('input');
+          input.type = 'file';
+          input.multiple = true;
+          input.accept = '.json';
+
+          input.onchange = e => {
+            files = e.target.files;
+          }
+
+          input.click();
+        }
+      },
+      two: {
+        icon: '<i class="fas fa-check"></i>',
+        label: "Import !",
+        callback: () => { stayOpen = true; importCharacter(files); }
+      }
+    },
+    default: "two",
+    render: html => console.log("Register interactivity in the rendered dialog"),
+    close: () => {
+      if (stayOpen) {
+        stayOpen = false;
+        d.render(true);
+      }
+    }
+
+  });
+  d.render(true);
+
+}
+
+Hooks.on("renderSidebarTab", function (e) {
+  if (e instanceof ActorDirectory) {
+    let actorsTab = document.getElementById("actors");
+    let footerOfActors = actorsTab.getElementsByClassName('directory-footer')[0];
+
+    let actionDiv = document.createElement('div');
+    let importButton = document.createElement('button');
+    let buttonIcon = document.createElement('i');
+
+    actionDiv.classList.add("action-buttons");
+    actionDiv.classList.add("flexrow");
+
+    buttonIcon.classList.add("fa-user");
+    buttonIcon.classList.add("fas");
+
+    importButton.classList.add("create-document");
+    importButton.classList.add("create-entry");
+    importButton.appendChild(buttonIcon);
+    importButton.textContent = "Import Miel's Actor";
+
+    importButton.addEventListener("click", importCharacterPopup);
+
+    actionDiv.appendChild(importButton);
+
+    footerOfActors.appendChild(actionDiv);
+  }
+})
+
+
+Hooks.on("ready", function () {
+});
---
+++ scripts/vampire_enum.js
@@ -0,0 +1,583 @@
+export const Clans = [
+    {
+        "id": 0,
+        "name": "Assamite"
+    },
+    {
+        "id": 1,
+        "name": "Brujah"
+    },
+    {
+        "id": 2,
+        "name": "Cappadocian"
+    },
+    {
+        "id": 3,
+        "name": "Setite"
+    },
+    {
+        "id": 4,
+        "name": "Gangrel"
+    },
+    {
+        "id": 5,
+        "name": "Giovanni"
+    },
+    {
+        "id": 6,
+        "name": "Lasombra"
+    },
+    {
+        "id": 7,
+        "name": "Malkavian"
+    },
+    {
+        "id": 8,
+        "name": "Nosferatu"
+    },
+    {
+        "id": 9,
+        "name": "Ravnos"
+    },
+    {
+        "id": 10,
+        "name": "Salubrien"
+    },
+    {
+        "id": 11,
+        "name": "Toreador"
+    },
+    {
+        "id": 12,
+        "name": "Tremere"
+    },
+    {
+        "id": 13,
+        "name": "Tzimisce"
+    },
+    {
+        "id": 14,
+        "name": "Ventrue"
+    },
+    {
+        "id": 15,
+        "name": "Caitiff"
+    }
+]
+
+export const Origins = [
+    {
+        "id": 0,
+        "name": "Anglais"
+    },
+    {
+        "id": 1,
+        "name": "Indien"
+    },
+    {
+        "id": 2,
+        "name": "Pologne"
+    },
+    {
+        "id": 3,
+        "name": "Irlande"
+    },
+    {
+        "id": 4,
+        "name": "Nigeria"
+    },
+    {
+        "id": 5,
+        "name": "Pakistan"
+    },
+    {
+        "id": 6,
+        "name": "Jamaïque"
+    }
+]
+
+export const Predations = {
+    0: 'Biberonneur',
+    1: 'Chat de gouttière',
+    2: 'Consensualiste',
+    3: 'Fermier',
+    4: 'Lamproie',
+    5: 'Marchand de sable',
+    6: 'Osiris',
+    7: 'Parasite domestique',
+    8: 'Reine de la nuit',
+    9: 'Succube',
+    10: 'Extortion',
+    11: 'Chausse-Trappe',
+    12: 'Faucheur',
+    13: 'Montero',
+    14: 'Pilleur de tombes',
+    15: 'Poursuivant',
+    16: 'Racketteur',
+}
+
+export const Attributes = {
+    'strength': 'Force',
+    'charisma': 'Charisme',
+    'intelligence': 'Intelligence',
+    'dexterity': 'Dextérité',
+    'manipulation': 'Manipulation',
+    'cunning': 'Astuce',
+    'stamina': 'Vigueur',
+    'composure': 'Sang-Froid',
+    'resolve': 'Résolution'
+}
+
+export const Skills = [
+    {
+        "id": 0,
+        "name": "firearms",
+        "fullname": "Armes à feu"
+    },
+    {
+        "id": 1,
+        "name": "animalken",
+        "fullname": "Animaux"
+    },
+    {
+        "id": 2,
+        "name": "academics",
+        "fullname": "Érudition"
+    },
+    {
+        "id": 3,
+        "name": "craft",
+        "fullname": "Artisanat"
+    },
+    {
+        "id": 4,
+        "name": "leadership",
+        "fullname": "Commandement"
+    },
+    {
+        "id": 5,
+        "name": "finance",
+        "fullname": "Finances"
+    },
+    {
+        "id": 6,
+        "name": "athletism",
+        "fullname": "Athlétisme"
+    },
+    {
+        "id": 7,
+        "name": "insight",
+        "fullname": "Empathie"
+    },
+    {
+        "id": 8,
+        "name": "investigation",
+        "fullname": "Investigation"
+    },
+    {
+        "id": 9,
+        "name": "brawl",
+        "fullname": "Bagarre"
+    },
+    {
+        "id": 10,
+        "name": "etiquette",
+        "fullname": "Étiquette"
+    },
+    {
+        "id": 11,
+        "name": "medicine",
+        "fullname": "Médecine"
+    },
+    {
+        "id": 12,
+        "name": "drive",
+        "fullname": "Conduite"
+    },
+    {
+        "id": 13,
+        "name": "streetwise",
+        "fullname": "Éxpérience de la rue"
+    },
+    {
+        "id": 14,
+        "name": "occult",
+        "fullname": "Occultisme"
+    },
+    {
+        "id": 15,
+        "name": "stealth",
+        "fullname": "Furtivité"
+    },
+    {
+        "id": 16,
+        "name": "intimidation",
+        "fullname": "Intimidation"
+    },
+    {
+        "id": 17,
+        "name": "politics",
+        "fullname": "Politique"
+    },
+    {
+        "id": 18,
+        "name": "larceny",
+        "fullname": "Larcin"
+    },
+    {
+        "id": 19,
+        "name": "performance",
+        "fullname": "Performance"
+    },
+    {
+        "id": 20,
+        "name": "science",
+        "fullname": "Sciences"
+    },
+    {
+        "id": 21,
+        "name": "melee",
+        "fullname": "Mêlée"
+    },
+    {
+        "id": 22,
+        "name": "persuasion",
+        "fullname": "Persuasion"
+    },
+    {
+        "id": 23,
+        "name": "technology",
+        "fullname": "Technologies"
+    },
+    {
+        "id": 24,
+        "name": "survival",
+        "fullname": "Survie"
+    },
+    {
+        "id": 25,
+        "name": "subterfuge",
+        "fullname": "Subterfuge"
+    },
+    {
+        "id": 26,
+        "name": "awareness",
+        "fullname": "Vigilance"
+    }
+]
+
+export const Specializations = {
+    0: ["Arbalètes", "Vente", "Fabrication", "Chargement manuel de munitions", "Dégainage rapide", "Snipers", "Tirs acrobatiques"],
+    1: ["Entraînement à l'attaque", "Chats", "Chiens", "Fauconnerie", "Chevaux", "Apaisement", "Rats", "Serpents", "Entraînement aux tours", "Loups"],
+    2: ["Architecture", "Littérature anglaise", "Histoire de l'art", "Histoire", "Journalisme", "Philosophie", "Recherche", "Enseignement", "Théologie"],
+    3: ["Menuiserie", "Gravure", "Design", "Peinture", "Sculpture", "Couture", "Forge d'armes"],
+    4: ["Ordres", "Inspiration", "Discours", "Praxis", "Dynamique d'équipe", "Meute de guerre"],
+    5: ["Estimation", "Secteur bancaire", "Marchés noirs", "Manipulation des devises", "Oeuvres d'art", "Malversations", "Blanchiment d'argent", "Bourse"],
+    6: ["Acrobaties", "Tir à l'arc", "Escalade", "Endurance", "Saut", "Parkour", "Natation", "Lancer"],
+    7: ["Ambitions", "Désirs", "Détection des mensonges", "Émotions", "Réconfort", "Interrogatoire", "Motivations", "Phobies", "Vices"],
+    8: ["Criminologie", "Déduction", "Médecine légale", "Personnes disparues", "Meurtres", "Mystères paranormaux", "Analyse du trafic sur internet"],
+    9: ["Animaux", "Mortels armés", "Bagarres de bar", "Lutte", "Descendants", "Matchs de sport de combat", "Mortels désarmés", "Loup-Garous", "Sous forme de la Bête de Protéisme"],
+    10: ["Anarchs", "Camarilla", "Célébrités", "Corporations", "Elysium", "Étiquette féodale", "Milliardaires", "Société secrète"],
+    11: ["Premiers soins", "Hématologie", "Pathologies infectieuses", "Pharmacopée", "Phlébotomie", "Chirurgie", "Traumatologie", "Médecine vétérinaire"],
+    12: ["Véhicules tout-terrain", "Fuite", "Motos", "Courses de rue", "Cascades", "Filature", "Camions", "Voitures anciennes"],
+    13: ["Trafic d'armes", "Marché noir", "Corruption", "Drogues", "Recel", "Gangs", "Tags", "Réputation", "Prostitution", "Survie urbaine"],
+    14: ["Alchimie", "Magie du sang", "Fées", "Fantômes", "Grimoires", "Infernalisme", "Mages", "Nécromancie", "Nodisme", "Parapsychologie", "Vaudou", "Loups-Garous"],
+    15: ["Embuscades", "Foules", "Déguisement", "Se cacher", "Filature", "Déplacement silencieux", "Ville", "Milieu sauvage"],
+    16: ["Racket", "Insultes", "Interrogatoire", "Coercition physique", "Baston de regards", "Menaces voilées"],
+    17: ["Anarchs", "Camarilla", "Gouvernement d'une ville", "Gouvernement d'un clan", "Diplomatie", "Médias", "Politique internationale", "Politique nationale", "Politique régionale"],
+    18: ["Alarmes", "Contrefaçon", "Grand Theft Auto", "Effraction", "Crochetage", "Pickpocket", "Coffres-forts", "Analyse des mesures de sécurité"],
+    19: ["Comédie", "Danse", "Théâtre", "Batterie", "Guitare", "Claviers", "Poésie", "Allocutions publiques", "Rap", "Chant", "Violon", "Instruments à vent"],
+    20: ["Astronomie", "Biologie", "Chimie", "Démolition", "Ingénierie", "Génétique", "Géologie", "Mathématiques", "Physique"],
+    21: ["Haches", "Chaînes", "Massue", "Escrime", "Désarmement", "Garrots", "Armes improvisées", "Couteux", "Pieux", "Épées"],
+    22: ["Marchandage", "Baratin", "Interrogatoire", "Plaidoirie", "Négociation", "Rhétorique"],
+    23: ["Artillerie", "Codage", "Assemblage d'ordinateur", "Extraction de données", "Piratage", "Réseaux", "Téléphones", "Système de surveillance"],
+    24: ["Désert", "Chasse", "Jungle", "Pistage", "Pièges", "Abris", "Exploration urbaine", "Forêt"],
+    25: ["Bluff", "Apparence humaine", "Mensonges en béton", "Innocence", "Arnaque", "Séduction"],
+    26: ["Embuscades", "Camouflage", "Objets cachées", "Ouïe", "Intuition", "Odorat", "Vue", "Pièges", "Milieu sauvage"],
+}
+
+export const Disciplines = [
+    {
+        id: 0,
+        name: 'animalism'
+    },
+    {
+        id: 1,
+        name: 'auspex'
+    },
+    {
+        id: 2,
+        name: 'celerity'
+    },
+    {
+        id: 3,
+        name: 'dominate'
+    },
+    {
+        id: 4,
+        name: 'fortitude'
+    },
+    {
+        id: 5,
+        name: 'oblivion'
+    },
+    {
+        id: 6,
+        name: 'obfuscate'
+    },
+    {
+        id: 7,
+        name: 'presence'
+    },
+    {
+        id: 8,
+        name: 'protean'
+    },
+    {
+        id: 9,
+        name: 'potence'
+    },
+    {
+        id: 10,
+        name: 'sorcery'
+    },
+]
+
+export const ClansDisciplines = {
+    0: [10, 2, 6],
+    1: [2, 9, 7],
+    2: [1, 4, 5],
+    3: [6, 7, 8],
+    4: [0, 4, 8],
+    5: [1, 4, 5],
+    6: [3, 5, 9],
+    7: [1, 6, 3],
+    8: [0, 6, 9],
+    9: [0, 6, 7],
+    10: [1, 3, 4],
+    11: [1, 2, 7],
+    12: [1, 3, 10],
+    13: [0, 3, 8],
+    14: [3, 4, 7],
+    15: []
+}
+
+export const Powers = {
+    0: {
+        1: ["Lier le famulus", "Sentir la Bête"],
+        2: ["Murmures Sauvages"],
+        3: ["Dompter la Bête", "Festin Animal"],
+        4: ["Soumission de l'Esprit"],
+        5: ["Hégémonie animale", "Lâcher la Bête"],
+        "amalgames": [{
+            "level": 1,
+            "disc": 4,
+            "power_level": 2,
+            "name": "Bêtes Endurantes"
+        },
+        {
+            "level": 2,
+            "disc": 8,
+            "power_level": 5,
+            "name": "Union de la Terre",
+            "need": "Fusion avec la Terre"
+        }],
+        "prerequisites": []
+    },
+    1: {
+        1: ["Sens Accru", "Sentir l'Invisible"],
+        2: ["Prémonition"],
+        3: ["Lecture de l'Âme", "Partage des Sens"],
+        4: ["Psychométrie"],
+        5: ["Clairvoyance", "Télépathie"],
+        "amalgames": [{
+            "level": 1,
+            "disc": 4,
+            "power_level": 2,
+            "name": "Valeren"
+        },
+        {
+            "level": 2,
+            "disc": 5,
+            "power_level": 2,
+            "name": "Prémonition Mortelle"
+        },
+        {
+            "level": 3,
+            "disc": 6,
+            "power_level": 4,
+            "name": "Dissimulation"
+        }],
+        "prerequisites": []
+    },
+    2: {
+        1: ["Grâce Féline", "Réflexes Éclairs"],
+        2: ["Rapidité"],
+        3: ["Fulgurance", "Traversée"],
+        4: ["Breuvage d'Élégance", "Tir Infaillible"],
+        5: ["Fraction de Seconde", "Frappe Éclair"],
+        "amalgames": [],
+        "prerequisites": []
+    },
+    3: {
+        1: ["Brouillage Mémoriel", "Contrainte"],
+        2: ["Faveur du Dominator", "Hypnose"],
+        3: ["Altération Mémorielle"],
+        4: ["Rationalisation"],
+        5: ["Décret Ultime", "Manipulation des Masses"],
+        'amalgames': [{
+            "level": 3,
+            "disc": 1,
+            "power_level": 5,
+            "name": "Soulager l'Âme Bestiale"
+        },
+        {
+            "level": 3,
+            "disc": 1,
+            "power_level": 5,
+            "name": "Possession"
+        },
+        {
+            "level": 1,
+            "disc": 7,
+            "power_level": 4,
+            "name": "Voix Impérieuse"
+        },
+        {
+            "level": 2,
+            "disc": 8,
+            "power_level": 2,
+            "name": "Vicissitude"
+        }
+        ],
+        "prerequisites": [
+            {
+                "need": "Hypnose",
+                "power_level": 3,
+                "name": "Injonction Latente"
+            }
+        ]
+    },
+    4: {
+        1: ["Résilience", "Espit Résolu"],
+        2: ["Résistance"],
+        3: ["Défier les Fléaux", "Fortifier le Masque Intérieur"],
+        4: ["Breuvage d'Endurance"],
+        5: ["Chair de Marbre", "Douleur Salvatrice"],
+        'amalgames': [{
+            "level": 1,
+            "disc": 1,
+            "power_level": 2,
+            "name": "Obéah"
+        },
+        {
+            "level": 2,
+            "disc": 5,
+            "power_level": 3,
+            "name": "Festin des Passions"
+        }],
+        "prerequisites": []
+    },
+    5: {
+        1: ["Ancre Liante", "Des Cendres aux Cendres", "Jeu d'Ombres", "Vision Abyssale"],
+        2: ["Invocation des ombres", "Où le Linceul s'Amincit"],
+        3: ["Aura de Putréfaction", "Sombre Vision", "Toucher des Abysses"],
+        4: ["Linceul Stygien", "Peste Nécrotique"],
+        5: ["Avatar Ténébreux", "Chemin des Abysses", "Esprit Flétri", "Skuld Accomplie"],
+        'amalgames': [],
+        "prerequisites": []
+    },
+    6: {
+        1: ["Manteau d'Ombre", "Silence de la Mort"],
+        2: ["Passage Invisible"],
+        3: ["Masque aux Mille Visages", "Fantôme dans la Machine"],
+        4: ["Disparition"],
+        5: ["Imposture", "Masquer l'Assemblée"],
+        'amalgames': [{
+            "level": 2,
+            "disc": 0,
+            "power_level": 3,
+            "name": "Essaim Serviteur"
+        },
+        {
+            "level": 2,
+            "disc": 3,
+            "power_level": 2,
+            "name": "Aliénation"
+        }],
+        "prerequisites": []
+    },
+    7: {
+        1: ["Intimider", "Révérence"],
+        2: ["Baiser Persistant"],
+        3: ["Envoûtement", "Regard Terrifiant"],
+        4: ["Convocation"],
+        5: ["Magnétisme Médiatique", "Majesté"],
+        "amalgames": [{
+            "level": 1,
+            "disc": 6,
+            "power_level": 2,
+            "name": "Chimérie"
+        },
+        {
+            "level": 2,
+            "disc": 6,
+            "power_level": 3,
+            "name": "Fata Morgana"
+        },
+        {
+            "level": 3,
+            "disc": 9,
+            "power_level": 3,
+            "name": "Étincelle de Rage"
+        }],
+        "prerequisites": []
+    },
+    8: {
+        1: ["Poids Plume", "Yeux de la Bête"],
+        2: ["Armes Bestiales"],
+        3: ["Change-Forme", "Fusion avec la Terre"],
+        4: ["Métamorphose"],
+        5: ["Forme de Brume", "Coeur sans Entrave"],
+        "amalgames": [],
+        "prerequisites": [{
+            "need": "Vicissitude",
+            "power_level": 3,
+            "name": "Sculpture de la Chair"
+        },
+        {
+            "need": "Vicissitude",
+            "power_level": 4,
+            "name": "Forme Monstrueuse"
+        },
+        {
+            "need": "Fusion avec la Terre",
+            "power_level": 5,
+            "name": "Union de la Terre",
+            "amalgame": true
+        }]
+    },
+    9: {
+        1: ["Bond Surhumain", "Corps Léthal"],
+        2: ["Prouesse"],
+        3: ["Ingurgitation Brutale", "Poigne Incroyable"],
+        4: ["Breuvage de Puissance"],
+        5: ["Frappe Télurique", "Poing de Caïn"],
+        "amalgames": [{
+            "level": 2,
+            "disc": 5,
+            "power_level": 2,
+            "name": "Les Bras d'Arhiman"
+        }],
+        "prerequisites": []
+    },
+    10: {
+        1: ["Goût du Sang", "Vitae Corrosive"],
+        2: ["Éteindre la vitae"],
+        3: ["Maturation du Sang", "Toucher du Scorpion"],
+        4: ["Vol de Vitae"],
+        5: ["Caresse de Baal", "Chaudron de Sang"],
+        "amalgames": [],
+        "prerequisites": []
+    }
+}
